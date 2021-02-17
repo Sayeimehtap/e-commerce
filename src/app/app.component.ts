982f6b84f698e1a85eed1a7a6b3ac6ec817faaa5
sayeimehtap <sayeimehtap@gmail.com>
2021-02-17 23:02:33 +0300

    menu and sidemenu edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilService } from './util.service';
+import { menuController } from '@ionic/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -24,12 +26,23 @@ export class AppComponent implements OnInit {
 
   constructor(
     private utilService: UtilService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
+    this.selectedIndex = 1;
     this.utilService.getMenuState().subscribe(menuState => {
       this.isMenuEnabled = menuState;
 
     });
+  }
+
+  navigate(path, selectedId) {
+    this.selectedIndex = selectedId;
+    this.router.navigate([path]);
+  }
+
+  onToggleMenu() {
+    menuController.toggle();
   }
 }
