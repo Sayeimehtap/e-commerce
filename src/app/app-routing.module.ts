8ca9d4ce651f648556d39bb9e2a61599df395d33
sayeimehtap <sayeimehtap@gmail.com>
2021-02-18 14:01:10 +0300

    checkout created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,10 @@ const routes: Routes = [
     path: 'favorite',
     loadChildren: () => import('./favorite/favorite.module').then( m => m.FavoritePageModule)
   },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./checkout/checkout.module').then( m => m.CheckoutPageModule)
+  },
 ];
 
 @NgModule({
